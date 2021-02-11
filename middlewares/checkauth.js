c8498c55f1ad7cdf92cca2be19096ae901ad6657
ufeanei <akaranjoh@yahoo.com>
2021-02-11 05:20:17 +0100

    git misbehaving

--- middlewares/checkauth.js
+++ middlewares/checkauth.js
@@ -2,7 +2,9 @@ import configs from "../config/env";
 const env = process.env.NODE_ENV || "development";
 export const checkauth = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1]; // this contain token
+    const token = req.headers.authorization.split(
+      " "
+    )[1]; /**this contain token */
     let decodedData;
     if (token) {
       decodedData = jwt.verify(token, configs[env].tksecret);
